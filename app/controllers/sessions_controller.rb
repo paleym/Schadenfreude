4f7df18862b665b5e5c234e4ae6ee5989190ab40
ohhdear <shannomm@colorado.edu>
2013-12-05 11:22:46 -0700

    added validations in user model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+		user = User.find_by_email(params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password]) #auth doesn't work, you were looking up how to make the auth method work. Sign in machinery still does not work yet.
+			#
+		else
+			flash[:flash] = 'Invalid email/password combination'
+			render 'new'
+		end
 	end
 
 	def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
 	EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
-	validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
-	validates :password, :confirmation => true
+	validates :email, :presence => true, :uniqueness => true, format: { with: EMAIL_REGEX } , length: { maximum: 50 }
+	validates :password, :confirmation => true, length: { minimum: 8 }
+	validates :firstName, presence => true, length: { maximum: 50 }
+	validates :lastName, presence => true, length: { maximum: 50 }
 	
 	attr_accessible :firstName, :lastName, :email, :password, :birthday
 end
