0688cb8e6bafa2d7afc0dfb424a039948be20ced
ohhdear <shannomm@colorado.edu>
2013-12-04 18:55:57 -0700

    added signin page/sessions controller Not fully implemented

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Schadenfreude::Application.routes.draw do
   resources :users
 	#match ':controller(/:action(/:id(.:format)))'
 	root :to => "users#main"
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
 end
