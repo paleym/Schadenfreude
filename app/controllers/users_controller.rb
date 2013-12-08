1455eb702e1a40a57a78736c278a63ac000ed3d6
Matthew Paley <paleym@colorado.edu>
2013-12-07 17:50:55 -0700

    adding session elements, and basic logout functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,49 @@
 class UsersController < ApplicationController
   def new
-  	@user = User.new
+    @user = User.new
   end
   def create
-  	@user = User.new(params[:user])
-  	if @user.save
-  		flash[:notice] = "You have successfully signed up for Schadenfreude!"
-  		flash[:color] = "valid"
-  	else
-  		flash[:notice] = "Form is invalid!"
-  		flash[:color] = "invalid"
-  	end
-  	session[:email] = @user.email
-  	params[:id] = @user.id.to_s
-  	@user.save
-  	redirect_to ('/users/' + params[:id])			#users_path(params[:id])
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:notice] = "You have successfully signed up for Schadenfreude!"
+      flash[:color] = "valid"
+    else
+      flash[:notice] = "Form is invalid!"
+      flash[:color] = "invalid"
+    end
+    session[:id] = @user.id.to_s
+    session[:email] = @user.email
+    session[:view] = "home"
+    params[:id] = @user.id.to_s
+    @user.save
+    redirect_to ('/users/' + params[:id])
   end
   def show
-  	@user = User.find(:first, :conditions => {:email => session[:email]})
-  	if (params[:id] != @user.id.to_s)
-	  	params[:id] = @user.id.to_s
-	  	redirect_to ('/users/' + params[:id])
-	  end
+    if (session[:view] == "home")
+      @user = User.find(:first, :conditions => {:email => session[:email]})
+      if (params[:id] != @user.id.to_s)
+        params[:id] = @user.id.to_s
+        redirect_to ('/users/' + params[:id])
+      end
+    elsif (session[:view] == "search")
+      @user = User.find(:first, :conditions => {:id => session[:searchfor]})
+    elsif (session[:view] == "logout")
+      @user = nil
+      session[:email] = nil
+      session[:id] = nil
+      params[:action] = index
+      redirect_to ('/users')
+    else
+      flash[:notice] = "You must log in before viewing User's profile pages!"
+      flash[:color] = "invalid"
+      redirect_to ('/users')
+    end
   end
   def index
-  	@users = User.all
+    @users = User.all
+  end
+  def search
+    
   end
   def main
   end
