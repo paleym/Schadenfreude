e6c4d542a06654ecd4f8444da71d31bb5abd3ec2
ohhdear <shannomm@colorado.edu>
2013-12-09 22:55:21 -0700

    User signin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131208010310) do
+ActiveRecord::Schema.define(:version => 20131210040715) do
 
   create_table "users", :force => true do |t|
     t.string   "firstName"
@@ -23,6 +23,9 @@ ActiveRecord::Schema.define(:version => 20131208010310) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "remember_token"
   end
+
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
