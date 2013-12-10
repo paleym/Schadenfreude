d87281bfa7a8c6070dab57654c8efef45cb1a2d6
ohhdear <shannomm@colorado.edu>
2013-12-10 15:02:48 -0700

    logout functionality, proper redirects

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save()
+      sign_in @user
       flash[:notice] = "You have successfully signed up for Schadenfreude!"
       flash[:color] = "valid"
     else
