0fce58098fd61c7d56bcea228ad835f1b13099de
Matthew Paley <paleym@colorado.edu>
2013-12-13 16:16:38 -0700

    working on edit information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,6 +41,17 @@ class UsersController < ApplicationController
   def edit
   	@user = User.find(session[:id])
   end
+  def update
+  	@user = User.find(session[:id])
+  	if @user.update_attributes!(params)	
+	    flash[:notice] = "Profile Information Updated!"
+      flash[:color] = "valid"
+	  else
+	  	flash[:notice] = "Problem saving information, Profile information not updated!"
+	    flash[:color] = "invalid"
+	  end
+    redirect_to user_path(@user)
+  end
   def main
   end
   def search
